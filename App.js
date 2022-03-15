ca9e958e5f8a94c4eeb7f425a6a51d2475b08d61
119140037 - Joy Ravelo Tarigan <56099222+joyrav09@users.noreply.github.com>
2022-03-15 20:15:23 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {NavigationContainer} from "@react-navigation/native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import Home from './src/pages/Home';
+import Detail from './src/pages/Detail';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+      }}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Detail" component={Detail} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
